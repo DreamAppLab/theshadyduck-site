23d7609d2dd976af34791ff6179b83f8655a4d06
Eddie Skehan <eddie@dreamapplab.com>
2026-07-07 11:40:17 -0400

    Use time-based revalidation instead of force-dynamic for better performance

--- app/spottings/page.tsx
+++ app/spottings/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import SpottingsMaps from "@/components/SpottingsMaps";
 import { getAllApprovedSightings } from "@/lib/sightings";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 export default async function SpottingsPage() {
   const sightings = await getAllApprovedSightings();
